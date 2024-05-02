6bfeba052641d9f1f5060ce798b308a26d551238
EliTE08 <memeinsta84@gmail.com>
2024-05-02 07:59:46 -0400

    All Functions Now Working
    All functions are now (I believe) fully working, also made some code optimizations.

--- main.py
+++ main.py
@@ -1,51 +1,70 @@
 import time
 
 def add(addWord, addDefinition):
-    dictionaryFile = open("dictionary.txt", "a")
-    dictionaryFile.write("{" + str(addWord) + ", " + str(addDefinition) + "} ")
+    with open("dictionary.txt", "a") as dictionaryFile:
+        dictionaryFile.write("{" + str(addWord) + ", " + str(addDefinition) + "}\n")
+    main()
 
 def read():
-    dictionaryFile = open("dictionary.txt", "r")
-    print(dictionaryFile.read())
+    with open("dictionary.txt", "r") as dictionaryFile:
+        print(dictionaryFile.read())
+    main()
 
 def remove(removeWord):
-    dictionaryFile = open("dictionary.txt", "r+")
-    readFile = dictionaryFile.read()
-    wordIndex = readFile.index(str(removeWord))
-    endBracketIndex = (readFile[wordIndex:]).index("}")
-    dictionaryFile.write(readFile[0: wordIndex-1] + readFile[endBracketIndex:])
+    with open("dictionary.txt", "r") as dictionaryFile:
+        lines = dictionaryFile.readlines()
+    with open("dictionary.txt", "w") as dictionaryFile:
+        for line in lines:
+            print(line)
+            if removeWord not in line:
+                dictionaryFile.write(line)
+    main()
+
+def check(checkWord):
+    with open("dictionary.txt", "r") as dictionaryFile:
+        lines = dictionaryFile.readlines()
+        for line in lines:
+            if checkWord in line:
+                print(line)
+    main()
+
 
 def main():
-    userInput = ""
-    while(userInput != "exit"):
-        userInput = input("Would you like to add, remove, check, read, or exit the dictionary?")
-        if(userInput == "exit"):
-            break
-        elif (userInput == "add"):
-            word = input("Enter the word: ")
-            definition = input("Enter the definition: ")
-            confirm = input(f"To confirm, you would like to add the word \"{word}\", with the definition \"{definition}\"? (y/n)")
-            if(confirm == "y"):
-                add(word, definition)
-            elif(confirm == "n"):
-                print("Okay.")
-                time.sleep(3)
-                main()
-        elif (userInput == "remove"):
-            word = input("Enter the word: ")
-            confirm = input(f"To confirm, you would like to remove the word \"{word}\"? (y/n)")
-            if(confirm == "y"):
-                remove(word)
-            elif(confirm == "n"):
-                print("Okay.")
-                time.sleep(3)
-                main()
+    userInput = input("Would you like to add, remove, check, read, or exit the dictionary?")
+    if(userInput == "exit"):
+        exit()
+    elif (userInput == "add"):
+        word = input("Enter the word: ")
+        definition = input("Enter the definition: ")
+        confirm = input(f"To confirm, you would like to add the word \"{word}\", with the definition \"{definition}\"? (y/n)")
+        if(confirm == "y"):
+            add(word, definition)
+        elif(confirm == "n"):
+            print("Okay.")
+            time.sleep(3)
+            main()
+    elif (userInput == "remove"):
+        word = input("Enter the word: ")
+        confirm = input(f"To confirm, you would like to remove the word \"{word}\"? (y/n)")
+        if(confirm == "y"):
             remove(word)
-        elif (userInput == "check"):
-            print("Check")
-        elif (userInput == "read"):
-            read()
-        else:
-            print("Invalid Choice!")
+        elif(confirm == "n"):
+            print("Okay.")
+            time.sleep(3)
+            main()
+    elif (userInput == "check"):
+        word = input("Enter the word: ")
+        confirm = input(f"To confirm, you would like to check the word \"{word}\"? (y/n)")
+        if(confirm == "y"):
+            check(word)
+        elif(confirm == "n"):
+            print("Okay.")
+            time.sleep(3)
+            main()
+    elif (userInput == "read"):
+        read()
+    else:
+        print("Invalid Choice!")
+        main()
 
 main()
